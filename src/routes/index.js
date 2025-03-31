0c754df2865d65637aeb0667d2600b9c28b6d5ef
TranQuoc <tranquoc1301@gmail.com>
2025-03-31 11:28:12 +0700

    fix routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const userRoutes = require("./user");
 const activityRoutes = require("./activity");
 const commentRoutes = require("./comment");
 const reviewRoutes = require("./review");
+const attractionRoutes = require("./attraction");
 const articleRoutes = require("./article");
 const router = express.Router();
 
@@ -12,7 +13,7 @@ router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/review", reviewRoutes);
 router.use("/activity", activityRoutes);
-
+router.use("/attractions", attractionRoutes);
 router.use("/comment", commentRoutes);
 router.use("/article", articleRoutes);
 
